f86f20d34661d2bc7f6ee5f36505baa004b55d54
Thomas Clauzel <t.clauzel@gmail.com>
2019-05-14 13:03:28 +0200

    Update scanner.sh

--- scanner.sh
+++ scanner.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+# ------------------------------------------------
+# author : 0code
+# title anon_ftp_finder.sh
+# depenfencies : nmap
+# description : a useful script to discover and try anonymous login on ftp server 
+# ------------------------------------------------
 echo "                                                             
  _____             _____ _____ _____ _____ _       _         
 |  _  |___ ___ ___|   __|_   _|  _  |   __|_|___ _| |___ ___ 
 |     |   | . |   |   __| | | |   __|   __| |   | . | -_|  _|
 |__|__|_|_|___|_|_|__|    |_| |__|  |__|  |_|_|_|___|___|_|  
-                                                             
-
+                                                            
 "
 echo "scan network for ftp server"
 range="78.192.0.0/16"
