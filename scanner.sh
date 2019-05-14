2b884fac904e369b7ce90d47236c4a4b35f08405
Thomas Clauzel <t.clauzel@gmail.com>
2019-05-14 09:07:49 +0200

    Create scanner.sh

---
+++ scanner.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo "scan network for ftp server"
+#range="192.168.1.1/24"
+range="90.130.70.1/24"
+echo $range will be scanned
+rm tmp.txt
+rm liste_ftp.txt
+rm liste_open.txt
+rm ip_ftp_open.txt
+echo "------------------"
+nmap -p 21 $range >> liste_ftp.txt
+grep -B 4 open liste_ftp.txt  >> liste_open.txt
+grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" liste_open.txt >> ip_ftp_open.txt
+cat ip_ftp_open.txt
+echo "------------------"
+echo "test ftp server for anonymous user"
+# boucle sur les ip de ipftpopen 
+cat ip_ftp_open.txt |  while read output
+do
+    echo "check for $output"
+    nmap --script ftp-anon $output > tmp.txt
+# Anonymous FTP login allowed
+string1="Anonymous FTP login allowed"
+	if grep -qF "$string1" tmp.txt;then
+    		echo "Found it"
+		grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" tmp.txt >> vulnerables_ftp.txt
+	else
+    		echo "Sorry this string not in file"
+	fi
+done
